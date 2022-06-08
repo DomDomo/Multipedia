4c579f6397233a12278c3bacc83b09213081a050
DomDomo <g.domis02@gmail.com>
2022-06-08 07:18:14 +0100

    Add basic search page with Material UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,32 @@
-import './App.css';
+import { Grid, IconButton, InputBase, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={6} sx={{ mb: 3 }}>
+          <Typography variant="h2">Multipedia</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="What would you like to know?"
+            style={{ minWidth: "70vh" }}
+          />
+          <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Grid>
+      </Grid>
     </div>
   );
 }
