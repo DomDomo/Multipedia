10e2784aef3f9cf552a56134e320e38d6e0d9d74
DomDomo <g.domis02@gmail.com>
2022-06-08 02:22:45 +0100

    Add Django REST framework and CORS exception requirements

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
+    'rest_framework',
+
     'multipedia',
     'urban'
 ]
@@ -49,6 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:8000'
 ]
 
 ROOT_URLCONF = 'backend.urls'
