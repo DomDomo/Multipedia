0ec42c6c67090490dbef1658cbb9b052c1e4d249
DomDomo <g.domis02@gmail.com>
2022-07-19 02:23:08 +0300

    Fix nested link and ugly plural bugs in Wiki API call

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -32,22 +32,66 @@ export const urbanRequest = (payload) => {
     .catch((err) => console.log(err));
 };
 
+// const findFirstParagraph = (summary, wikiText) => {
+//   let summaryWords = summary.replace(/<[^>]*>?/gm, "").split(" "); // Remove HTML tags
+//   summaryWords = summaryWords.filter((word) => !word.match(/[^\w\s]/g)); // Remove words with punctuation
+//   const paragraphs = wikiText.split("\n");
+
+//   console.log(paragraphs);
+//   console.log(summaryWords);
+
+//   // Choose the paragraps that has all the words that the summary has
+//   const firstParagraph = paragraphs.filter((p) =>
+//     summaryWords.every((word) => p.includes(word))
+//   )[0];
+//   console.log(firstParagraph);
+
+//   return firstParagraph;
+// };
+
+const findNestedLinks = (text) => {
+  let levels = [];
+  let depth = 0;
+  [...text].forEach((c) => {
+    if (c === "[") depth++;
+    if (depth >= 2) levels.push(c);
+    if (depth >= 2 && c === "]") levels.push("~~");
+    if (c === "]") depth--;
+  });
+  return levels.join("").split("~~").slice(0, -1);
+};
+
 const addLinksToSummary = (summaryData, linkData) => {
-  const pageId = Object.keys(linkData.query.pages)[0];
-  const wikiPage =
-    linkData["query"]["pages"][pageId]["revisions"]["0"]["slots"]["main"]["*"];
-  const regexp = /\[\[(.*?)\]\]/g;
-  const allLinksInPage = [...wikiPage.matchAll(regexp)].map((link) =>
+  const wikiText = linkData["parse"]["wikitext"]["*"];
+  const doubleBrackets = /\[\[(.*?)\]\]/g;
+  const linksInParagraph = [...wikiText.matchAll(doubleBrackets)].map((link) =>
     link[0].slice(2, -2).split("|").at(-1).trim()
   );
 
-  const noDupOrEmptyLinks = [
-    ...new Set(allLinksInPage.filter((link) => link.length > 0)),
+  // Find all links from received wikitext in the [[SOME_LINK]] format
+  let allLinks = [
+    ...new Set(linksInParagraph.filter((link) => link.length > 1)),
   ];
 
-  noDupOrEmptyLinks.forEach((element) => {
-    summaryData = summaryData.replace(element, `[${element}]`);
+  // Filter out all non-relevant links by making sure that the words
+  // in them are the same ones in the summary text
+  let possibleLinks = allLinks.filter((link) => summaryData.includes(link));
+  possibleLinks.sort((a, b) => b.length - a.length);
+
+  // Add link format to the summary text
+  // Cheese is a dairy... => Cheese is a [dairy]...
+  possibleLinks.forEach((link) => {
+    summaryData = summaryData.replace(link, `[${link}]`);
   });
+
+  // Remove nested links: [Washington, [D.C.]] => [Washington, D.C.]
+  findNestedLinks(summaryData).forEach((link) => {
+    summaryData = summaryData.replace(link, `${link.slice(1, -1)}`);
+  });
+
+  // Fix plural links by adding the "s" in the link
+  console.log(summaryData);
+  summaryData = summaryData.replace(/]s/g, "s]");
 
   return summaryData;
 };
@@ -55,12 +99,12 @@ const addLinksToSummary = (summaryData, linkData) => {
 export const wikiRequest = (payload) => {
   const titleOptions = {
     method: "GET",
-    url: `https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=search&srsearch=${payload}`,
+    url: `https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&list=search&srsearch=${payload}`,
   };
 
   const wikiSummaryURL = "https://en.wikipedia.org/api/rest_v1/page/summary/";
   const wikiLinkURL =
-    "https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=revisions&rvprop=content&rvslots=*&titles=";
+    "https://en.wikipedia.org/w/api.php?origin=*&format=json&action=parse&prop=wikitext&section=0&page=";
 
   return axios.request(titleOptions).then((res) => {
     let firstItemTitle = res.data["query"]["search"][0]["title"];
