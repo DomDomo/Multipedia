56a44c9ed95d93879ffa4130242987081adfc421
DomDomo <g.domis02@gmail.com>
2022-09-21 05:49:14 +0300

    Add progress bar when loading

--- frontend/src/components/LogoBar.js
+++ frontend/src/components/LogoBar.js
@@ -4,9 +4,17 @@ import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import FindInPageIcon from "@mui/icons-material/FindInPage";
 import { Link } from "react-router-dom";
 
-const LogoBar = () => {
+const LogoBar = ({ progress }) => {
+  const progressBarSyle = `linear-gradient(to right, #1AE6C0 ${progress}%, transparent 0%) 100% 1`;
   return (
-    <AppBar style={{ backgroundColor: "#171D2E" }} position="sticky">
+    <AppBar
+      style={{
+        backgroundColor: "#171D2E",
+        borderBottom: "2px solid",
+        borderImage: progressBarSyle,
+      }}
+      position="sticky"
+    >
       <Toolbar sx={{ justifyContent: "flex-start", alignItems: "center" }}>
         <Box
           display="flex"
--- frontend/src/components/ResultPage.js
+++ frontend/src/components/ResultPage.js
@@ -16,53 +16,71 @@ import {
 } from "../util/api";
 import { objectIsEmpty } from "../util/helper";
 
+const defaultResult = {
+  google: {},
+  urban: {},
+  wiki: {},
+  twitter: {},
+  progress: 0,
+};
+const icrementNum = 25;
+
 const ResultPage = () => {
   const { search } = useLocation().state;
 
-  const [fullResult, setFullResult] = useState({
-    google: {},
-    urban: {},
-    wiki: {},
-    twitter: {},
-  });
+  const [fullResult, setFullResult] = useState(defaultResult);
 
+  // API requests
   useEffect(() => {
-    setFullResult({
-      google: {},
-      urban: {},
-      wiki: {},
-      twitter: {},
-    });
+    setFullResult(defaultResult);
     googleRequest(search).then((data) =>
       setFullResult((fullResult) => ({
         ...fullResult,
         google: data,
+        progress: fullResult.progress + icrementNum,
       }))
     );
     urbanRequest(search).then((data) =>
       setFullResult((fullResult) => ({
         ...fullResult,
         urban: data,
+        progress: fullResult.progress + icrementNum,
       }))
     );
     wikiRequest(search).then((data) =>
       setFullResult((fullResult) => ({
         ...fullResult,
         wiki: data,
+        progress: fullResult.progress + icrementNum,
       }))
     );
     twitterRequest(search).then((data) =>
       setFullResult((fullResult) => ({
         ...fullResult,
         twitter: data,
+        progress: fullResult.progress + icrementNum,
       }))
     );
   }, [search]);
 
+  // Progress bar turn off hook
+  useEffect(() => {
+    if (fullResult.progress < 100) return;
+
+    const intervalId = setInterval(() => {
+      setFullResult((fullResult) => ({
+        ...fullResult,
+        progress: 0,
+      }));
+    }, 500);
+
+    return () => clearInterval(intervalId);
+  }, [fullResult.progress]);
+
   return (
     <Box style={{ width: "100%" }}>
-      <LogoBar />
-      <Box sx={{ margin: "0 auto", maxWidth: "50rem", minWidth: "24rem" }}>
+      <LogoBar progress={fullResult.progress} />
+      <Box sx={{ margin: "0 auto", maxWidth: "45rem", minWidth: "24rem" }}>
         <Grid
           container
           spacing={0}
@@ -83,26 +101,21 @@ const ResultPage = () => {
               alignItems="center"
               justifyContent="center"
             >
-              {!objectIsEmpty(fullResult.google) && (
-                <Grid item xs={10}>
-                  <GoogleCard google={fullResult.google} />
-                </Grid>
-              )}
-              {!objectIsEmpty(fullResult.urban) && (
-                <Grid item xs={10}>
-                  <UrbanCard urban={fullResult.urban} />
-                </Grid>
-              )}
-              {!objectIsEmpty(fullResult.wiki) && (
-                <Grid item xs={10}>
-                  <WikiCard wiki={fullResult.wiki} />
-                </Grid>
-              )}
-              {!objectIsEmpty(fullResult.twitter) && (
-                <Grid item xs={10}>
+              <Grid item xs={10}>
+                <GoogleCard google={fullResult.google} />
+              </Grid>
+              <Grid item xs={10}>
+                <UrbanCard urban={fullResult.urban} />
+              </Grid>
+
+              <Grid item xs={10}>
+                <WikiCard wiki={fullResult.wiki} />
+              </Grid>
+              <Grid item xs={10}>
+                {!objectIsEmpty(fullResult.twitter) && (
                   <TwitterCard twitter={fullResult.twitter} />
-                </Grid>
-              )}
+                )}
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
